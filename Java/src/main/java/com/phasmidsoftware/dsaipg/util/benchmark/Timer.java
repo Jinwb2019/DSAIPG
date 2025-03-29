eaa04ad71d91d95ba05a4aad6c97c1aef991b95c
Jinwb2019 <59530895+Jinwb2019@users.noreply.github.com>
2025-03-28 23:40:22 -0400

    Update Timer.java
    HW6

--- Java/src/main/java/com/phasmidsoftware/dsaipg/util/benchmark/Timer.java
+++ Java/src/main/java/com/phasmidsoftware/dsaipg/util/benchmark/Timer.java
@@ -69,9 +69,17 @@ public class Timer {
      * @return the average milliseconds per repetition.
      */
     public <T, U> double repeat(int n, boolean warmup, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
-        // TO BE IMPLEMENTED : note that the timer is running when this method is called and should still be running when it returns.
-         return 0;
-        // END SOLUTION
+        for (int i = 0; i < n; i++) {
+            T t = supplier.get();
+            if (preFunction != null) t = preFunction.apply(t);
+            U result = function.apply(t);
+            if (postFunction != null) postFunction.accept(result);
+            lap();
+        }
+        pause();
+        final double result = meanLapTime();
+        resume();
+        return result;
     }
 
     /**
@@ -244,9 +252,7 @@ public class Timer {
      * @return the number of ticks for the system clock. Currently defined as nano time.
      */
     private static long getClock() {
-        // TO BE IMPLEMENTED 
-         return 0;
-        // END SOLUTION
+        return System.nanoTime();
     }
 
     /**
@@ -257,9 +263,7 @@ public class Timer {
      * @return the corresponding number of milliseconds.
      */
     private static double toMillisecs(long ticks) {
-        // TO BE IMPLEMENTED 
-         return 0;
-        // END SOLUTION
+        return ticks / 1_000_000.0;
     }
 
     /**
